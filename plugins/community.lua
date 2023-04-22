766b12997b25a0b2bce2e6e1f510fb8066ec4cbe
Akio <akio@littlespin.studio>
2023-04-22 19:55:19 +0200

    chore(theme): adds catppuccin theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,35 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        ts_rainbow2 = true,
+        telescope = true,
+        nvimtree = false,
+        ts_rainbow = false,
+        aerial = true,
+        dap = { enabled = true, enable_ui = true },
+        mason = true,
+        neotree = true,
+        notify = true,
+        semantic_tokens = true,
+        symbols_outline = true,
+        which_key = true
+      },
+    },
+  },
+  { import = "astrocommunity.pack.typescript" }
 }
